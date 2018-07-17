ecc77d3a86ff9513d7c8387b0ad170eba6818e13
John Kwening <kwening.john@gmail.com>
2018-07-16 23:38:34 -0400

    refacter: Use properly sized images
    Resized all restaurant images to 300px width to improve loading
    performances by using smaller sized images based on dimensions used
    across multiple viewport size.
    
    Performace = 82%.
    
    Next - handle offscreen images by lazy loading those 'below the fold'.

--- js/helper.js
+++ js/helper.js
@@ -145,7 +145,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    return (`/img/${restaurant.photograph}_300w.jpg`);
   }
 
   /**
